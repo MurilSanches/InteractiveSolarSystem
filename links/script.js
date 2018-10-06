d28393fbfffa68ce31e700dc04f5097f0d4affe1
MurilSanches <murilosanches2018@gmail.com>
2018-10-06 18:42:13 -0300

    Mudancas
    Foi alterado algumas coisas e implementacao de novos coisas

--- links/script.js
+++ links/script.js
@@ -1,4 +1,8 @@
-var c = document.getElementById("myCanvas");
+var c = document.getElementById("orbitaMercurio");
+
+c.style.index = "1";
 var ctx = c.getContext("2d");
+ctx.style.border = "2px solid white";
 ctx.fillStyle = "white";
-ctx.fillRect(0,0,150,75);
+ctx.arc(45%,20%,0,0,true);
+ctx.fill();
